67646dade39ac77ef2cfe4d50c51021d4eacf822
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-05-25 15:45:24 +0800

    Initialize ph-shoes-backend Spring Project

---
+++ ph-shoes-backend/src/main/java/com/nimbly/phshoesbackend/PhShoesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nimbly.phshoesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhShoesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhShoesBackendApplication.class, args);
+	}
+
+}
---
+++ ph-shoes-backend/src/test/java/com/nimbly/phshoesbackend/PhShoesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbly.phshoesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhShoesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
